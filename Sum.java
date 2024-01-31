9b9a0187ffe865ed99c6d8efafa8df4ce2318b41
Md Younus Talukder <141204197+younus-talukder@users.noreply.github.com>
2024-02-01 03:26:47 +0600

    Sum of two element in java

---
+++ Sum.java
@@ -0,0 +1,13 @@
+public class Sum {
+    public static void main(String[] args )
+    {
+        int[] arr = {1,2,3,4,5};
+                int sum=0;
+        for(int i = 0;i<5;i++) {
+            sum = sum + arr[i];
+
+
+            System.out.println(sum);
+        }
+    }
+}
