c9fb3eedb6748e3c66fbeb2d1339d1943c4b135e
younus-talukder <141204197+younus-talukder@users.noreply.github.com>
2024-01-31 14:56:14 +0600

    for loop with String

---
+++ animal.java
@@ -0,0 +1,11 @@
+package animal;
+
+public class animal {
+	public static void main(String[] args) {
+		String[] animal= {"cat","dog","horse"};
+		for(String i : animal)
+		System.out.println(i);
+	}
+	
+
+}
